5b43b91f37a231cbc9ceccf4bb78d23f6fa6560e
skiziks <nicolas.lerond@gmail.com>
2022-10-14 22:04:22 +0200

    mise a jour

--- traitement.php
+++ traitement.php
@@ -1,17 +1,9 @@
-<html>
-  <head>
-    <title>réponse du serveur</title>
-  </head>
-  <body>
-  <?php print "Hello dear ";
+<?php print "Hello dear ";
     print $_POST["last_name"] ;
     print " ";
     print $_POST["first_name"];
     print "<BR><BR><HR>";
     print " your mail adress is : ";
     print $_POST["mail"];
-    print " Your message have been send to me, thanks :)"; 
-    ?>
-  </body>
-</html> 
-
+    print " Your message have been send to me, thanks"; 
+?>
