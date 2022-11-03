d6904ae8d068ad699f186c4a6b25cf1676d20eb2
skiziks <nicolas.lerond@gmail.com>
2022-11-03 12:43:53 +0100

    v1

--- README.md
+++
@@ -1,1 +0,0 @@
-This is my CV website for a school project
--- traitement.php
+++ traitement.php
@@ -1,9 +1,22 @@
-<?php print "Hello dear ";
-    print $_POST["last_name"] ;
-    print " ";
-    print $_POST["first_name"];
-    print "<BR><BR><HR>";
-    print " your mail adress is : ";
-    print $_POST["mail"];
-    print " Your message have been send to me, thanks"; 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body id="php_page">
+    <div class="php_form">
+        <?php print "Hello dear ";
+            print $_POST["last_name"] ;
+            print " ";
+            print $_POST["first_name"];
+            print "<BR><BR><HR>";
+            print " your mail adress is : ";
+            print $_POST["mail"];
+            print " Your message have been send to me, thanks"; 
+        ?>
+    </div>
+</body>
+</html>
