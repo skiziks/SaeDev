1766613d633ebdd901e147cc55e9207e846de120
Skiziks <nicolas.lerond@gmail.com>
2023-01-25 12:32:47 +0100

    update

--- traitement.php
+++ traitement.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Contact</title>
 </head>
 <body id="php_page">
@@ -17,6 +18,9 @@
             print $_POST["mail"];
             print " Your message have been send to me, thanks"; 
         ?>
+    </div>
+    <div class="back_top">
+        <a href="index.html">Return Home</a>
     </div>
 </body>
 </html>
